a4b60ace8bcba92633c265ed7d697786e79b6f6e
Yann Sionneau <yann@minet.net>
2011-05-09 10:06:18 +0200

    translated some French names into English

--- forward.c
+++ forward.c
@@ -16,36 +16,36 @@
 extern pcap_t *handle;
 extern eth_addr_t my_mac_addr;
 
-void forward(const u_char *packet, size_t taille, eth_addr_t to) {
+void forward(const u_char *packet, size_t size, eth_addr_t to) {
 
 	struct eth_hdr *header_ethernet;
 	u_char *new_packet;
 	int ret;
 	size_t i;
 
-	printf("On forward\n");
-	printf("\t taille = %ld\n", taille);
+	printf("We forward\n");
+	printf("\t size of the packet = %ld\n", size);
 
 
 	printf("A dump of what we received from the network : \n");
-	for (i = 0 ; i < taille ; i++) {
+	for (i = 0 ; i < size ; i++) {
 		printf("%02X ", packet[i]);
 	}
 
-	new_packet = malloc(sizeof(u_char) * taille);
+	new_packet = malloc(sizeof(u_char) * size);
 
 	if (new_packet == NULL) {
 		perror("Error : cannot allocate memory for a new packet");
 		exit(1);
 	}
 
-	memcpy(new_packet, packet, taille);
+	memcpy(new_packet, packet, size);
 	header_ethernet = (struct eth_hdr *)new_packet;
 
 	memcpy(header_ethernet->eth_src.data, my_mac_addr.data, ETH_ADDR_LEN);
 	memcpy(header_ethernet->eth_dst.data, to.data, ETH_ADDR_LEN);
 
-	ret = pcap_sendpacket(handle, new_packet, taille);
+	ret = pcap_sendpacket(handle, new_packet, size);
 
 	if (ret < 0) {
 		printf("\t ERROR : failed to forward packet\n");
@@ -53,7 +53,7 @@ void forward(const u_char *packet, size_t taille, eth_addr_t to) {
 	}
 
 	printf("A dump of what we sent to the network : \n");
-	for (i = 0 ; i < taille ; i++) {
+	for (i = 0 ; i < size ; i++) {
 		printf("%02X ", new_packet[i]);
 	}
 
