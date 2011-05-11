b72a4b4f01308d31ea8e0b592e8ba8a9987e3b9e
Yann Sionneau <yann@minet.net>
2011-05-11 22:42:09 +0200

    working plugins, test.so changes last byte of each packet by a @

--- test.c
+++ test.c
@@ -3,6 +3,17 @@
 char name[] = "test";
 char author[] = "Yann Sionneau";
 
+int do_match(const char *packet)
+{
+  return 1;
+}
+
+void process_packet(char *packet, size_t size)
+{
+  printf("\n[test] We process the packet\n");
+  packet[size - 1] = '@';
+}
+
 int startup(void) {
 
 	printf("Ok on est lancé !\n");
--- test2.c
+++ test2.c
@@ -3,6 +3,16 @@
 char name[] = "test2";
 char author[] = "Yannou";
 
+int do_match(const char *packet)
+{
+  return 0;
+}
+
+void process_packet(char *packet)
+{
+  printf("[test2] We process the packet\n");
+}
+
 int startup(void) {
 
 	printf("Ok test2 !\n");
